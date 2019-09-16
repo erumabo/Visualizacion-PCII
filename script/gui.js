7940725498f2781ca550eeb005af67e957650ebf
mabo <mabo.daniel@gmail.com>
2019-09-16 16:43:03 -0600

    Zoom and panning reviewd, click y select added --Cambio en la forma del zoom/panning, eliminado el uso de las funciones transform y scale para poder transformar coordenadas del sketch y la pantalla sin problema --Agregado el control de seleccion de nodo y sus aristas --Agregado funciones para manejo de archivos: pendiente el (re)generar el grafo con ellos

--- script/gui.js
+++ script/gui.js
@@ -1,9 +1,26 @@
-function abrirArchivo(event){
-  var input = event.target;
-  var reader = new FileReader();
+function abrirAristas(event){
+  let input = event.target;
+  let reader = new FileReader();
+  $('#inAristasL').html(input.files[0].name);
   reader.onload = function(){
-  var text = reader.result;
-    console.log(text);
+    let lines = reader.result.split('\n');
+    for(const line of lines){
+      console.log(line.match(/[A-Z]/g));
+      console.log(line.match(/[0-9]+/g));
+    }
+  };
+  reader.readAsText(input.files[0]);
+};
+
+function abrirNodos(event){
+  let input = event.target;
+  let reader = new FileReader();
+  $('#inVertexL').html(input.files[0].name);
+  reader.onload = function(){
+    let lines = reader.result.split('\n');
+    for(const line of lines){
+      console.log(line.match(/[A-Z]/g));
+    }
   };
   reader.readAsText(input.files[0]);
 };
--- script/processing.js
+++ script/processing.js
@@ -1,33 +1,50 @@
 let grafo = new Springy.Graph();
 grafo.addNodes('0','1','2','3','4','5','6','7','8','9','10','11','12','13','14');
-grafo.addEdges(
-  ['0','2'],
-  ['0','9'],
-  ['2','4'],
-  ['2','9'],
-  ['2','11'],
-  ['4','11'],
-  ['9','11'],
-  ['14','9'],
-  ['14','11']
-);
+grafo.addEdges(['0','2'],['0','9'],['2','4'],['2','9'],['2','11'],['4','11'],['9','11'],['14','9'],['14','11']);
+
 let layout = new Springy.Layout.ForceDirected(grafo, 40.0, 400.0, 0.5);
 
-let canvas = undefined;
-let change = true, animate=true;
+let canvas;
+let change = true, animate=true, select=false;
 let zoom = 1;
 let mx,my,dx,dy,px,py;
+let nodo = [undefined,undefined];
 
-function render(){
-  background(206);
-  layout.eachEdge((e,s)=>{
-    line(zoom*s.point1.p.x, zoom*s.point1.p.y, zoom*s.point2.p.x, zoom*s.point2.p.y);
-  });
+function eucl(x,y,u,v){
+  return sqrt((x-u)*(x-u)+(y-v)*(y-v));
+}
+
+function nodoE(mx,my){
+  select = false;
   layout.eachNode((n,p)=>{
-    ellipse(zoom*p.p.x,zoom*p.p.y,2);
+    let tx=tX(zoom*p.p.x);
+    let ty=tY(zoom*p.p.y);
+    if(eucl(tx,ty,mx,my)<=(4*zoom)){
+      select=true;
+      nodo = n.id;
+    }
   });
 }
 
+function tX(x){return (x+width/2)*zoom + dx;}
+function tY(y){return (y+height/2)*zoom + dy;}
+
+function render(){
+  background(206);
+  fill(0,0,255);
+  stroke(0,0,255);
+  layout.eachEdge((e,s)=>{
+    if(select && (e.source.id==nodo || e.target.id==nodo)){ stroke(0,0,0,255);strokeWeight(zoom);}
+    else{ stroke(0,0,0,100); strokeWeight(1);}
+    line(tX(zoom*s.point1.p.x),tY(zoom*s.point1.p.y),tX(zoom*s.point2.p.x),tY(zoom*s.point2.p.y));
+  });
+  noStroke();
+  layout.eachNode((n,p)=>{
+    if(select && n.id == nodo) fill(0,0,0,255);
+    else fill(0,0,0,100);
+    ellipse(tX(zoom*p.p.x),tY(zoom*p.p.y),4*zoom);
+  });
+}
 
 function setup(){
   px = py = dx = dy = 0;
@@ -36,10 +53,6 @@ function setup(){
 }
 
 function draw(){
-  translate(displayWidth/2,displayHeight/2);
-  scale(zoom);
-  translate(dx/zoom,dy/zoom);
-  
   if(animate){
     layout.tick(0.03);
     Springy.requestAnimationFrame(()=>{
@@ -51,6 +64,13 @@ function draw(){
   }
 }
 
+function mouseWheel(event){
+  console.log(event.delta);
+  zoom -= (event.delta/10);
+  if(zoom>100)zoom=100;
+  if(zoom<1)zoom=1;
+}
+
 function keyPressed(){
   if(key==='+'){ 
     zoom +=0.5;
@@ -59,17 +79,18 @@ function keyPressed(){
   }
   if(key==='-'){
     zoom -=0.5;
-    if(zoom<0) zoom=0;
+    if(zoom<1) zoom=1;
     return false;
   }
   return true;
 }
 
 function mousePressed(){
-  mx = mouseX;
-  my = mouseY;
-  px = dx;
-  py = dy;
+  nodoE(mouseX,mouseY);
+    mx = mouseX;
+    my = mouseY;
+    px = dx;
+    py = dy;
 }
 function mouseDragged(){
   change = true;
